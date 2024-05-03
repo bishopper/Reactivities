7857636d5e0a1b2800364b63b0795aab9e36f9bb
Mohammad Behnam <muhammad.behnam@yahoo.com>
2024-05-03 20:27:32 +0330

    Create ActivitiesService.cs

---
+++ Reactivities.Application/Services/Implementation/ActivitiesService.cs
@@ -0,0 +1,18 @@
+﻿using Reactivities.Application.Services.Interfaces;
+using Reactivities.Domain.Entities.User;
+using Reactivities.Domain.Interfaces;
+
+namespace Reactivities.Application.Services.Implementation;
+public class ActivitiesService : IActivitiesService
+{
+    private readonly IActivitiesRepository _activitiesRepository;
+    public ActivitiesService(IActivitiesRepository activitiesRepository)
+    {
+        _activitiesRepository = activitiesRepository;
+    }
+
+    public async Task<List<Activity>> GetAllActivitiesAsync()
+    {
+        return await _activitiesRepository.GetAllActivitiesAsync();
+    }
+}
