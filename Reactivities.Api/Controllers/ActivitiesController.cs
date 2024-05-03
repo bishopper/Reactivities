73cd1ba25cef3bcd08e9115ad594b6ea4eace616
Mohammad Behnam <muhammad.behnam@yahoo.com>
2024-05-03 20:27:42 +0330

    Update ActivitiesController.cs

--- Reactivities.Api/Controllers/ActivitiesController.cs
+++ Reactivities.Api/Controllers/ActivitiesController.cs
@@ -1,4 +1,19 @@
-﻿namespace Reactivities.Api.Controllers;
+﻿using Microsoft.AspNetCore.Mvc;
+using Reactivities.Application.Services.Interfaces;
+
+namespace Reactivities.Api.Controllers;
 public class ActivitiesController : BaseApiController
 {
+    private readonly IActivitiesService _activitiesService;
+    public ActivitiesController(IActivitiesService activitiesService)
+    {
+        _activitiesService = activitiesService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllActivities()
+    {
+        var activities = await _activitiesService.GetAllActivitiesAsync();
+        return new JsonResult(activities);
+    }
 }
