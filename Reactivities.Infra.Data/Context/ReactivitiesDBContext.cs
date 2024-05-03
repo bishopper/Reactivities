f95e3a50fee4b76bcb545dcc86cc9e5215a1054c
Mohammad Behnam <muhammad.behnam@yahoo.com>
2024-05-03 19:01:17 +0330

    Add project files.

---
+++ Reactivities.Infra.Data/Context/ReactivitiesDBContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Reactivities.Domain.Entities.User;
+
+namespace Reactivities.Infra.Data.Context;
+public class ReactivitiesDBContext : DbContext
+{
+    #region Constructor
+
+    public ReactivitiesDBContext(DbContextOptions<ReactivitiesDBContext> options) : base(options)
+    {
+
+    }
+
+    #endregion
+
+
+    #region User
+
+    public DbSet<Activity> Activities { get; set; }
+
+    #endregion
+}
