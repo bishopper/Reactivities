8fdab8cbe70815bbf96f2fd4592740f2064d6c13
Mohammad Behnam <muhammad.behnam@yahoo.com>
2024-05-03 20:27:18 +0330

    Create DependencyContainer.cs

---
+++ Reactivities.Infra.IOC/DependencyContainer/DependencyContainer.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Reactivities.Application.Services.Implementation;
+using Reactivities.Application.Services.Interfaces;
+using Reactivities.Domain.Interfaces;
+using Reactivities.Infra.Data.Repositories;
+
+namespace Reactivities.Infra.IOC.DependencyContainer;
+public static class DependencyContainer
+{
+    public static void RegisterServices(this IServiceCollection services)
+    {
+        // Repositories
+        services.AddScoped<IActivitiesRepository, ActivitiesRepository>();
+
+
+        // services
+        services.AddScoped<IActivitiesService, ActivitiesService>();
+    }
+}
