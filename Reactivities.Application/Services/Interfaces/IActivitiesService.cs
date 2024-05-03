e354fbef69a6160828b6c85f36862e9632071eaa
Mohammad Behnam <muhammad.behnam@yahoo.com>
2024-05-03 20:27:30 +0330

    Create IActivitiesService.cs

---
+++ Reactivities.Application/Services/Interfaces/IActivitiesService.cs
@@ -0,0 +1,7 @@
+﻿using Reactivities.Domain.Entities.User;
+
+namespace Reactivities.Application.Services.Interfaces;
+public interface IActivitiesService
+{
+    Task<List<Activity>> GetAllActivitiesAsync();
+}
