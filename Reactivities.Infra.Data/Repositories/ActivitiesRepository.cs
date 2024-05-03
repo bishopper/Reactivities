e334e4493e886cbea6878b25b0a329d251272545
Mohammad Behnam <muhammad.behnam@yahoo.com>
2024-05-03 20:27:21 +0330

    Create ActivitiesRepository.cs

---
+++ Reactivities.Infra.Data/Repositories/ActivitiesRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Reactivities.Domain.Entities.User;
+using Reactivities.Domain.Interfaces;
+using Reactivities.Infra.Data.Context;
+
+namespace Reactivities.Infra.Data.Repositories;
+public class ActivitiesRepository : IActivitiesRepository
+{
+    public readonly ReactivitiesDBContext _dbContext;
+
+    public ActivitiesRepository(ReactivitiesDBContext reactivitiesDBContext)
+    {
+        _dbContext = reactivitiesDBContext;
+    }
+
+
+    public async Task<List<Activity>> GetAllActivitiesAsync()
+    {
+        return await _dbContext.Activities.ToListAsync();
+    }
+}
