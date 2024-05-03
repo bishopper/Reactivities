e319299c1fa52894f01c6b1604fdf9a2b2f87feb
Mohammad Behnam <muhammad.behnam@yahoo.com>
2024-05-03 20:27:27 +0330

    Create IActivitiesRepository.cs

---
+++ Reactivities.Domain/Interfaces/IActivitiesRepository.cs
@@ -0,0 +1,7 @@
+﻿using Reactivities.Domain.Entities.User;
+
+namespace Reactivities.Domain.Interfaces;
+public interface IActivitiesRepository
+{
+    Task<List<Activity>> GetAllActivitiesAsync();
+}
